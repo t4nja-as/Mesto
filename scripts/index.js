526d0110afc6c8e566cc1d18ab986f38fb6b5d31
tatiana.astashina <astash5@mail.ru>
2022-06-14 00:13:24 +0300

    сделать попап

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,51 @@ popupCloseButton.addEventListener('click', closePopup);
 
 function openPopup(event) {
     popup.classList.add('popup_opened');
-    
+
 }
 
 function closePopup(event) {
     popup.classList.remove('popup_opened');
 }
-console.log(profileEditButton)
+console.log(profileEditButton);
+
+
+
+
+let formElement = document.querySelector('.popup__container');
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    let nameInput = document.querySelector('.popup__input_text_name');
+    let jobInput = document.querySelector('.popup__input_text_position');
+    let profileTitle = document.querySelector('.profile__title');
+    let profileSubtitle = document.querySelector('.profile__subtitle');
+
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+
+    closeEditForm()
+
+};
+
+function closeEditForm() { }
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+/*1 вариант
+function qs(selector) {
+    return document.querySelector(selector);
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    qs('.profile__title').textContent = qs('.popup__input_text_name').value;
+    qs('.profile__subtitle').textContent = qs('.popup__input_text_position').value;
+    closeEditForm()
+}
+
+function closeEditForm() { }
+formElement.addEventListener('submit', formSubmitHandler);*/
