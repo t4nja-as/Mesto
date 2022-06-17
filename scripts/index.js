5954bee96ea1b4062cb2cd7539c582a9b7e695bb
tatiana.astashina <astash5@mail.ru>
2022-06-17 23:35:27 +0300

    правки попапа

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,11 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.popup__input_text_name');
+let jobInput = document.querySelector('.popup__input_text_position');
+
 profileEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 
@@ -15,11 +20,6 @@ let formElement = document.querySelector('.popup__form');
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('.popup__input_text_name');
-    let jobInput = document.querySelector('.popup__input_text_position');
-    let profileTitle = document.querySelector('.profile__title');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
-
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
     closePopup()
